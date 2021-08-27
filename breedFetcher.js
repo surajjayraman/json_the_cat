03200d7ce842ec699f7b92109e35760d96be4f1f
surajjayraman <suraj.jayraman@gmail.com>
2021-08-26 20:01:34 -0400

    implemented Mocha Chai testing for Cats as a Service

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,35 +1,42 @@
 const request = require('request');
-const breedName = process.argv[2];
 
 //fetch breed content
-const fetchBreed = function(breedName) {
+const fetchBreedDescription = function(breedName, callback) {
   const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
     
   request(url, (error, response, body) => {
+    //if (error) {
+    //console.log('error:', error);
+    // }
     if (error) {
-      console.log('error:', error);
+      callback(`Failed to request details: ${error}`,null);
     }
     
-    console.log('body:', body);
-    console.log(typeof body);
+    //console.log('body:', body);
+    //console.log(typeof body);
 
     const data = JSON.parse(body);
-    console.log(data);
-    console.log(typeof data);
+    //console.log(data);
+    //console.log(typeof data);
     //print out breed description
     const breed = data[0];
-    if (!breed) {
-      console.log(`Failed to find breed ${breedName}`);
-      return false;
+    if (breed) {
+      callback(null,breed['description']);
+    } else {
+      callback(`Failed to find breed ${breedName}`,null);
     }
-    console.log(breed['description']);
-
-       
+             
   });
 
   return true;
 
+
 };
 
-fetchBreed(breedName);
+module.exports = {
+  fetchBreedDescription
+};
+
+
+
 
